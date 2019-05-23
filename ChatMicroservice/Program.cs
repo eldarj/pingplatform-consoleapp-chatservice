bf66cb062c55ab2a87cbbd559e8b226e6385e38b
eldarj <eldar.jah@gmail.com>
2019-05-23 18:30:27 +0200

    Init Chat microservice - console app

---
+++ ChatMicroservice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChatMicroservice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
